d4369f0b16ae7621a5f5e373f747e6f9de417b72
Ian Warrington <iwarrington@orangeline.com.au>
2017-02-20 12:10:44 +1100

    Fix for broken font loading under HTTPS

--- bundles/FontAsset.php
+++ bundles/FontAsset.php
@@ -13,7 +13,7 @@ class FontAsset extends AssetBundle
     public $sourcePath = '@hustshenl/metronic/assets';
 
     public $css = [
-    	  'http://fonts.googleapis.com/css?family=Raleway:400,700',
+    	  '///fonts.googleapis.com/css?family=Raleway:400,700',
           'global/plugins/font-awesome/css/font-awesome.min.css',
     ];
 }
